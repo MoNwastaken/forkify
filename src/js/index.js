c1a3a8766affbc984cd3d362bcef973bb5852ea0
Lanczi Dominik <nhcmon@gmail.com>
2020-01-22 15:32:33 +0100

    140

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
-import str from './models/Search';
-//import {add as a, multiply as m, ID} from './views/searchView';
-import * as searchView from './views/searchView'
+import axios from 'axios';
 
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error)
+    }
+
+}
+
+getResults('pasta');
