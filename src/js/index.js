ddfec013c0cf9d96cee8580e7bab991a0fdaf4d4
Lanczi Dominik <nhcmon@gmail.com>
2020-01-24 11:39:17 +0100

    147

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
 import * as searchView from './views/searchView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
@@ -11,6 +12,7 @@ import {elements, renderLoader, clearLoader} from './views/base';
 
 const state = {};
 
+// Search controller
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput();
@@ -46,3 +48,8 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result, goToPage);
     }
 });
+
+// Recipe controller 35626
+const r = new Recipe(35626);
+r.getRecipe();
+console.log(r);
