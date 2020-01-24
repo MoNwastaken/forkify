6530a652b86a5e5931766a56254da244ca45232b
Lanczi Dominik <nhcmon@gmail.com>
2020-01-24 11:56:25 +0100

    148

--- src/js/index.js
+++ src/js/index.js
@@ -26,12 +26,18 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4. Search for recipes
-        await state.search.getResults();
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
 
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result)
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result)
+        } catch (err) {
+            alert('Something went wrong while searching...');
+            clearLoader();
+        }
+
     }
 };
 
@@ -49,7 +55,33 @@ elements.searchResPages.addEventListener('click', e => {
     }
 });
 
-// Recipe controller 35626
-const r = new Recipe(35626);
-r.getRecipe();
-console.log(r);
+// Recipe controller
+const controlRecipe = async () => {
+    // Get ID from url
+  const id = window.location.hash.replace('#', '');
+  console.log(id);
+
+  if (id) {
+      // Prepare UI for changes
+
+      // Create new recipe object
+      state.recipe = new Recipe(id);
+
+      try {
+          // Get recipe data
+          await state.recipe.getRecipe();
+
+          // Calculate servings and time
+          state.recipe.calcTime();
+          state.recipe.calcServings();
+
+          // Render recipe
+          console.log(state.recipe);
+      } catch (err) {
+          alert('Error processing recipe!');
+      }
+
+  }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
